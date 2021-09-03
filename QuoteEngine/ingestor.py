a616b06905c17f69de3397e209c2f311ba55bbfd
SulimanCS <sulimanAlsarraf@gmail.com>
2021-09-03 20:52:43 +0300

    Implement TextIngestor

--- QuoteEngine/ingestor.py
+++ QuoteEngine/ingestor.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from quote import QuoteModel
 
 # TODO: docstring, pep8 pep 257
 # TODO: __str__
@@ -25,3 +26,46 @@ class IngestorInterface(ABC):
         """
         pass
 
+
+class TextIngestor(IngestorInterface):
+    """A text file type ingestor.
+
+    Text ingestor implements a parser to extract quotes from .txt files
+    """
+
+    ingestor_type = "txt"
+
+    def parse(self, path: str) -> list[QuoteModel]:
+        """Parse a file to get a list of quote models.
+
+        :param path: a path for the file to be ingested.
+        :return: a list of `quotemodels`.
+        """
+        quote_models = []
+        if self.can_ingest(path):
+            try:
+                # utf-8-sig encoding fixes a weird where
+                # letters would be read as symbols
+                with open(path, "r", encoding="utf-8-sig") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        quote = line.split("-")
+                        body = quote[0].strip()
+                        author = quote[1].strip()
+                        quote_model = QuoteModel(body, author)
+                        quote_models.append(quote_model)
+
+            except FileNotFoundError as e:
+                print(e)
+                return []
+            return quote_models
+        print(f"{self.__class__.__name__} object can't ingest {path}")
+        return []
+
+
+if __name__ == "__main__":
+    test = TextIngestor()
+    print(test.can_ingest("grger.txt"))
+    print(test.can_ingest("_data/DogQuotesTXT.few"))
+    quote_models = test.parse("_data/DogQuotes/DogQuotesTXT.txt")
+    pass
